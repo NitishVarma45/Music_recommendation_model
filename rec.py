7e34629d80035dad509d7d1f1dee86eab8b9a079
NitishVarma45 <codingnitish81@gmail.com>
2026-01-21 16:59:37 +0530

    Update rec.py

--- rec.py
+++ rec.py
@@ -4,22 +4,17 @@ import pandas as pd
 # Load saved files
 similarity = pickle.load(open('similarity.pkl', 'rb'))
 data = pickle.load(open('df.pkl', 'rb'))
-
+print(data['song'].head(50))
 def recommend(song_name, n_recommendations=20):
-    # Check if song exists
     if song_name not in data['song'].values:
         return "Song not found in dataset."
 
-    # Get index of the song
     idx = data[data['song'] == song_name].index[0]
 
-    # Get similarity scores
     distances = list(enumerate(similarity[idx]))
 
-    # Sort by similarity score (descending)
     distances = sorted(distances, key=lambda x: x[1], reverse=True)
 
-    # Collect recommendations
     recommended_songs = []
     for i in distances[1:n_recommendations+1]:
         recommended_songs.append(data.iloc[i[0]].song)
